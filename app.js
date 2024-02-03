56f18eb2b8a0b81f108f3c125d4bb2b0c8b515b9
Mohammed Safadi <mohsafadi@Mohammeds-MacBook-Air.local>
2024-02-03 15:22:59 +0400

    Creating the foundation of the page and applying the styling

---
+++ app.js
@@ -0,0 +1,19 @@
+//Defining main page components
+let startGameTitle = document.querySelector(".startGameTitle");
+let blueBox = document.querySelector(".blue");
+let redBox = document.querySelector(".red");
+let yellowBox = document.querySelector(".yellow");
+let greenBox = document.querySelector(".green");
+
+//Preset array for colors, this will be the reference that will be used by the random color generator.
+let colorArray=["blue", "red", "yellow", "green"];
+
+//Random color generator
+
+startGameTitle.addEventListener("click", function () {
+  return alert("clicked");
+});
+
+blueBox.addEventListener("click", function () {
+  return alert("clicked");
+});
